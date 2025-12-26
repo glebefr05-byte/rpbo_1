725e3aea28c738922fa62a8c2310f030f02315ac
Glebefr <glebefr07@yandex.ru>
2025-12-27 02:10:55 +0300

    OWASP A05 Commit

--- src/main/java/ru/mtuci/coursemanagement/config/WebConfig.java
+++ src/main/java/ru/mtuci/coursemanagement/config/WebConfig.java
@@ -3,13 +3,19 @@ package ru.mtuci.coursemanagement.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
+@EnableMethodSecurity
+@EnableWebSecurity
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedMethods("*")
+                .allowedMethods("GET");
+        registry.addMapping("/api/**")
+                .allowedMethods("GET", "POST", "PUT")
                 .allowedOrigins("*")
                 .allowedHeaders("*");
 
