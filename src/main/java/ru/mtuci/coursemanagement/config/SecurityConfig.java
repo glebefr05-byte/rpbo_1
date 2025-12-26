2b7bd6d8293d8cdf31b2fb1b0afb966afa8f2081
Glebefr <glebefr07@yandex.ru>
2025-12-27 02:06:49 +0300

    OWASP A01 Commit

---
+++ src/main/java/ru/mtuci/coursemanagement/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package ru.mtuci.coursemanagement.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(authz -> authz
+                .anyRequest().permitAll()
+            );
+        return http.build();
+    }
+}
